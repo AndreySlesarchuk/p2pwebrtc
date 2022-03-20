44e481701562f90d14c6e45f1c15599089e5be9e
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-03-20 13:06:01 +0300

    useWebRTC added

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-
 const io = require('socket.io')(server, {
     cors: {
         origin: '*'
     }
 });
 
-const ACTIONS = require("./src/socket/actions");
-const {version, validate} = require("uuid");
+const {version, validate} = require('uuid');
+
+const ACTIONS = require('./src/socket/actions');
 const PORT = process.env.PORT || 3001;
 
 function getClientRooms() {
@@ -33,7 +33,7 @@ io.on('connection', socket => {
         const {rooms: joinedRooms} = socket;
 
         if (Array.from(joinedRooms).includes(roomID)) {
-            return console.warn(`Already joined to ${roomID}`)
+            return console.warn(`Already joined to ${roomID}`);
         }
 
         const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
@@ -46,7 +46,7 @@ io.on('connection', socket => {
 
             socket.emit(ACTIONS.ADD_PEER, {
                 peerID: clientID,
-                createOffer: true
+                createOffer: true,
             });
         });
 
@@ -58,27 +58,55 @@ io.on('connection', socket => {
         const {rooms} = socket;
 
         Array.from(rooms)
+            // LEAVE ONLY CLIENT CREATED ROOM
+            .filter(roomID => validate(roomID) && version(roomID) === 4)
             .forEach(roomID => {
-                const clients = Array.from(io.socket.adapter.rooms.get(roomID) || []);
 
-                clients.forEach(clientID => {
-                    io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
-                        peerID: socket.id
+                const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+                clients
+                    .forEach(clientID => {
+                        io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+                            peerID: socket.id,
+                        });
+
+                        socket.emit(ACTIONS.REMOVE_PEER, {
+                            peerID: clientID,
+                        });
                     });
 
-                    socket.emit(ACTIONS.REMOVE_PEER, {
-                        peerID: clientID
-                    });
-                });
                 socket.leave(roomID);
             });
+
         shareRoomsInfo();
     }
 
     socket.on(ACTIONS.LEAVE, leaveRoom);
     socket.on('disconnecting', leaveRoom);
-    console.log('Socket connected');
-})
+
+    socket.on(ACTIONS.RELAY_SDP, ({peerID, sessionDescription}) => {
+        io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+            peerID: socket.id,
+            sessionDescription,
+        });
+    });
+
+    socket.on(ACTIONS.RELAY_ICE, ({peerID, iceCandidate}) => {
+        io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+            peerID: socket.id,
+            iceCandidate,
+        });
+    });
+
+});
+
+const publicPath = path.join(__dirname, 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 server.listen(PORT, () => {
     console.log('Server Started!')
